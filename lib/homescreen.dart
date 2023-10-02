17807d7bdab14e4ff08acdbe86ffe2cab854bd07
Dharmeshkatariya <katariyadharmesh658@gmail.com>
2023-10-02 07:57:30 +0530

    add filterdata

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -77,6 +77,29 @@ class _HomeScreenState extends State<HomeScreen> {
   _sortList() {
     mlist.sort((a, b) => a.compareTo(b));
     _filterList();
+    _sortedData();
+  }
+
+  _sortedData() {
+    var cli = ['CL1', "CL2", "Cl3", "CL4"];
+    List<UserDetails> userDetaillist = [];
+    List<int> age = [10, 20, 50, 60,];
+    for (int i = 0; i < cli.length; i++) {
+      UserDetails userDetails = UserDetails();
+      userDetails.name = cli[i];
+      userDetails.age = age[i];
+      userDetaillist.add(userDetails)
+      ;
+    }
+    filterData.clintlist.addAll(cli);
+
+    filterData.userDetail.addAll(userDetaillist);
+
+    // Make sure to set the filterData object to the class variable
+    // This ensures that the data is available in the build method
+    setState(() {
+      filterData = filterData;
+    });
   }
 
   List<NameData> finalList = [];
@@ -96,6 +119,7 @@ class _HomeScreenState extends State<HomeScreen> {
           NameData nameData = NameData();
           nameData.name = nameList[0][0];
           nameData.nameList = nameList1;
+
           finalList.add(nameData);
         }
         nameList.clear();
@@ -108,7 +132,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   final searchController = TextEditingController();
-
+  FilterData filterData = FilterData() ;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -119,83 +143,128 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               TextFormField(
                 onChanged: (value) {
-                  _searchName(value);
+                  _searchForFilterData(value);
                 },
                 controller: searchController,
                 decoration: InputDecoration(
-                    hintText: "search name",
+                    hintText: "search client cli data",
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
                     )),
               ),
+              // searchController.text.isNotEmpty
+              //     ? Expanded(
+              //   child: ListView.builder(
+              //       itemCount: searchList.length,
+              //       itemBuilder: (BuildContext context, int index) {
+              //         return Container(
+              //           padding: const EdgeInsets.symmetric(
+              //               horizontal: 10, vertical: 10),
+              //           margin: const EdgeInsets.symmetric(
+              //               horizontal: 10, vertical: 6),
+              //           decoration: BoxDecoration(
+              //             color: Colors.pink.shade50,
+              //             borderRadius: BorderRadius.circular(12),
+              //           ),
+              //           child: Text(searchList[index]),
+              //         );
+              //       }),
+              // )
+              //     : Expanded(
+              //   child: ListView.builder(
+              //       itemCount: finalList.length,
+              //       itemBuilder: (BuildContext context, int index) {
+              //         NameData nameData = finalList[index];
+              //         return Column(
+              //           children: [
+              //             GestureDetector(
+              //               onTap: () =>
+              //               {
+              //                 finalList[index].isExpand =
+              //                 !finalList[index].isExpand,
+              //                 setState(() {}),
+              //               },
+              //               child: Container(
+              //                 padding: const EdgeInsets.symmetric(
+              //                     horizontal: 10, vertical: 10),
+              //                 margin: const EdgeInsets.symmetric(
+              //                     horizontal: 10, vertical: 0),
+              //                 decoration: BoxDecoration(
+              //                   color: Colors.pink.shade50,
+              //                   borderRadius: BorderRadius.circular(12),
+              //                 ),
+              //                 child: Text(nameData.name),
+              //               ),
+              //             ),
+              //             ListView.builder(
+              //                 itemCount: nameData.nameList.length,
+              //                 physics:
+              //                 const NeverScrollableScrollPhysics(),
+              //                 shrinkWrap: true,
+              //                 itemBuilder:
+              //                     (BuildContext context, int index) {
+              //                   return Container(
+              //                     height: nameData.isExpand ? null : 0,
+              //                     padding: const EdgeInsets.symmetric(
+              //                         horizontal: 10, vertical: 10),
+              //                     margin: const EdgeInsets.symmetric(
+              //                         horizontal: 10, vertical: 6),
+              //                     decoration: BoxDecoration(
+              //                       color: Colors.pink.shade50,
+              //                       borderRadius:
+              //                       BorderRadius.circular(12),
+              //                     ),
+              //                     child: Text(nameData.nameList[index]),
+              //                   );
+              //                 }),
+              //           ],
+              //         );
+              //       }),
+              // ),
+
+              // Inside your HomeScreen class...
               searchController.text.isNotEmpty
                   ? Expanded(
-                    child: ListView.builder(
-                        itemCount: searchList.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 10),
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 6),
-                            decoration: BoxDecoration(
-                              color: Colors.pink.shade50,
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Text(searchList[index]),
-                          );
-                        }),
-                  )
-                  : Expanded(
-                      child: ListView.builder(
-                          itemCount: finalList.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            NameData nameData = finalList[index];
-                            return Column(
-                              children: [
-                                GestureDetector(
-                                  onTap: () => {
-                                    finalList[index].isExpand =
-                                        !finalList[index].isExpand,
-                                    setState(() {}),
-                                  },
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 10),
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 0),
-                                    decoration: BoxDecoration(
-                                      color: Colors.pink.shade50,
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Text(nameData.name),
-                                  ),
-                                ),
-                                ListView.builder(
-                                    itemCount: nameData.nameList.length,
-                                    physics:
-                                        const NeverScrollableScrollPhysics(),
-                                    shrinkWrap: true,
-                                    itemBuilder:
-                                        (BuildContext context, int index) {
-                                      return Container(
-                                        height: nameData.isExpand ? null : 0,
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 10, vertical: 10),
-                                        margin: const EdgeInsets.symmetric(
-                                            horizontal: 10, vertical: 6),
-                                        decoration: BoxDecoration(
-                                          color: Colors.pink.shade50,
-                                          borderRadius:
-                                              BorderRadius.circular(12),
-                                        ),
-                                        child: Text(nameData.nameList[index]),
-                                      );
-                                    }),
-                              ],
-                            );
-                          }),
-                    ),
+                child: ListView.builder(
+                    itemCount: searchListFilterData.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 6),
+                        decoration: BoxDecoration(
+                          color: Colors.pink.shade50,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Text(searchListFilterData[index]),
+                      );
+                    }),
+              ) :
+              Expanded(
+                child: ListView.builder(
+                    itemCount: filterData.clintlist.length,
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                        decoration: BoxDecoration(
+                          color: Colors.pink.shade50,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Column(
+                          children: [
+                            Text(filterData.clintlist[index],style: TextStyle(color: Colors.red),),
+                            Text(filterData.userDetail[index].age.toString(),style: TextStyle(color: Colors.red),),
+                          ],
+                        ),
+                      );
+                    }
+                ),
+              ),
+
             ],
           ),
         ),
@@ -203,6 +272,22 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+
+
+  List<String> searchListFilterData  =[] ;
+  _searchForFilterData(String str){
+    searchListFilterData.clear() ;
+    if(str.isNotEmpty){
+      for (int i = 0; i < filterData.clintlist.length; i++) {
+        if (filterData.clintlist[i].toLowerCase().contains(str.toLowerCase())) {
+          searchListFilterData.add(filterData.clintlist[i]);
+        }
+      }
+      setState(() {
+
+      });
+    }
+  }
   _searchName(String str) {
     searchList.clear();
     if (str.isNotEmpty) {
@@ -221,4 +306,17 @@ class NameData {
   String name = '';
   List<String> nameList = [];
   bool isExpand = false;
+}
+
+class FilterData {
+  List<String> clintlist = [];
+
+  List<UserDetails> userDetail = [];
+}
+
+
+class UserDetails {
+  String name = "";
+
+  int age = 10;
 }
